16efbfb582299bf160a7ffea914890378ed1a376
Oksana <axana13@gmail.com>
2024-09-23 18:46:55 +0200

    db

--- Backend/server.js
+++ Backend/server.js
@@ -4,7 +4,22 @@ import pg from "pg";
 
 const app = express();
 const PORT = 8000;
+const { Client } = pg;
 
 app.use(cors()); // Cross Origin Ressource Sharing
+
+app.get("/posts", async (req, res) => {
+  try {
+    const client = new Client({
+      connectionString: process.env.PG_URI,
+    });
+    await client.connect();
+    const results = await client.query("SELECT * FROM posts");
+    await client.end();
+    res.status(200).json(results.rows);
+  } catch (error) {
+    res.status(500).send(`Error fetching posts: ${error}`);
+  }
+});
 
 app.listen(PORT, () => console.log(`Server is running on port:${PORT}`));
