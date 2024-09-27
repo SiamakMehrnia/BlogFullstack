d2dbabbee9cacd98fd4d0df14423c8792c59ca5a
Oksana <axana13@gmail.com>
2024-09-27 20:06:41 +0200

    handling error status 404

--- Backend/controllers/blog.js
+++ Backend/controllers/blog.js
@@ -6,7 +6,7 @@ const getPosts = async (req, res, next) => {
     const results = await pool.query("SELECT * FROM posts");
     res.status(200).json(results.rows);
   } catch (error) {
-    next(new ErrorResponse(`Error fetching posts: ${error.message}`, 500));
+    next(error);
   }
 };
 
@@ -14,23 +14,24 @@ const getPostByID = async (req, res, next) => {
   try {
     const { id } = req.params;
     const results = await pool.query("SELECT * FROM posts WHERE id=$1", [id]);
+    if (results.rows.length === 0)
+      throw new ErrorResponse("Post not found.", 404);
     res.status(200).json(results.rows);
   } catch (error) {
-    next(new ErrorResponse(`Error fetching post: ${error.message}`, 500));
+    next(error);
   }
 };
 
 const createPost = async (req, res, next) => {
   try {
     const { title, author, content, cover } = req.body;
-
     const results = await pool.query(
       "INSERT INTO posts (title, author, content, cover) VALUES ($1, $2, $3, $4) RETURNING *",
       [title, author, content, cover]
     );
     res.status(201).json(results.rows[0]);
   } catch (error) {
-    next(new ErrorResponse(`Error creating post: ${error.message}`, 500));
+    next(error);
   }
 };
 
@@ -43,23 +44,26 @@ const updatePost = async (req, res, next) => {
       "UPDATE posts SET title=$1, author=$2, content=$3, cover=$4 WHERE id=$5 RETURNING *",
       [title, author, content, cover, id]
     );
+    if (results.rows.length === 0)
+      throw new ErrorResponse("Post not found.", 404);
     res.status(200).json(results.rows[0]);
   } catch (error) {
-    next(new ErrorResponse(`Error update post: ${error.message}`, 500));
+    next(error);
   }
 };
 
 const deletePost = async (req, res, next) => {
   try {
     const { id } = req.params;
-
     const results = await pool.query(
       "DELETE FROM posts WHERE id=$1 RETURNING *",
       [id]
     );
+    if (results.rows.length === 0)
+      throw new ErrorResponse("Post not found.", 404);
     res.status(200).json({ message: `Post was deleted.` });
   } catch (error) {
-    next(new ErrorResponse(`Error deleting post: ${error.message}`, 500));
+    next(error);
   }
 };
 
